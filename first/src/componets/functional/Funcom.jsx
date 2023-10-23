71afa8394a27d97e43eeeb61dcd153ad2498816f
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-10-23 11:49:46 +0530

    add

---
+++ first/src/componets/functional/Funcom.jsx
@@ -0,0 +1,4 @@
+function Funcom(){
+    return <h1>Functional componets</h1>
+}
+export default Funcom
--- first/src/main.jsx
+++ first/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import React from "react"
+import  ReactDOM  from "react-dom/client"
+import  Funcom from
+let x=<h1>What is this...?</h1>
+let arry=["urvish","jeel","nimesh"]
+let obj={name:"Nimesh"}
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+<React.StrictMode>
+{/* <h1>Hello</h1>
+<h1>{15+15}</h1>
+{x}
+{arry.map((e)=>{
+    return <h1>{e}</h1>
+})} */}
+{obj.name}
+</React.StrictMode>
 )
