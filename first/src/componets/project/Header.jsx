0f49f8992f711ab025becdd0fa4c8dd42379ca9d
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-10-26 15:02:47 +0530

    add

--- first/src/componets/project/Header.jsx
+++ first/src/componets/project/Header.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-     
+    <div className='d-flex justify-content-between   bg-primary text-white'>
+      <div className='m-3'>
+        <p className='fs-6 fw-bold'><span className='text-black fw-bold fs-6'>Email:-</span>devaliyanimesh61792@gmail.com </p>
+      </div>
+      <div className='m-3  '>
+        <p> <span className='text-black fw-bold fs-6'>Contact No:-</span>7041541797 </p>
+      </div>
+
     </div>
 
   )
