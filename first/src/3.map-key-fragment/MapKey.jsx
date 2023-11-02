f4d8e60faf66ab7a1b4f6ca212817893cec6b5c7
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-11-02 13:16:25 +0530

    add

--- first/src/3.map-key-fragment/MapKey.jsx
+++ first/src/3.map-key-fragment/MapKey.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useId } from 'react'
 
 export default function MapKey() {
     let namerr=["Nimesh","Saif","Latesh","Nayan"]
   return (
     <>    <div >
         {namerr.map((e,i)=>{
-            return <div  key={i} > <h1> {i+1}.{e}</h1></div>
+            let id=useId()
+        return <div  key={id} > <h1> {i+1}.{e}</h1></div>
         })}
     </div>
     </>
