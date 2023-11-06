65eca95a870cbc47ab823bd808e65d50e2b52a8c
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-11-06 12:18:33 +0530

    add

--- first/src/4.Pops/Fun/Propschange.jsx
+++ first/src/4.Pops/Fun/Propschange.jsx
@@ -12,6 +12,12 @@ export default function Propschange(props) {
 <h1>Hello, {changee}</h1>
 <Button onClick={()=>changename()} className=''>Click here</Button>
 </div>
+<hr />
+
+<div className='m-3'>
+<h1>Hello, {props.name}</h1>
+<Button onClick={()=>changenam()} className=''>Click here</Button>
+</div>
   </>
   )
 }
