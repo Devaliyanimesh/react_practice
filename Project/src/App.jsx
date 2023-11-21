6bee8ae563f1838b0d9abe2416400857ed847e0e
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-11-21 11:36:33 +0530

    add

---
+++ Project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Nav from './Navbar/Nav'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <>
+   <Nav/>
+   `</>
+  )
+}
+
+export default App
