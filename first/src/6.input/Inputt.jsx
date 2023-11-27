5cc5ce5e43b770780f3f95f4dba4c8cdf47cd3fd
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-11-27 11:54:36 +0530

    add

--- first/src/6.input/Inputt.jsx
+++ first/src/6.input/Inputt.jsx
@@ -3,41 +3,48 @@ import React, { useState } from 'react'
 
 export default function Inputt() {
     let [food, setfood] = useState("")
+    // four-->input ni value  store karva mate arry ma mukvani maultiple che atle
     let [foodList, setfoodList] = useState([])
-
+    // third--> input ni value kema male te jovani pachi tene hook ma mukvani and food ne input ma
     const change = (ele) => {
         setfood(ele.target.value);
     }
-const addvalue =()=>{
-    setfoodList([...foodList,food])
-    setfood("")
+     {/* five--> pachad ni value store karvani tena mate [spred opretor no use karvano ,food]
+    pachi input khali karvanu che atle setfood("") blank karvanu  */}
+    const addvalue = () => {
+        setfoodList([...foodList, food])
+        setfood("")
 
-}
+    }
     return (
         <>
-           
+
             <div className=' border border-5 border-primary w-25 m-5 p-2 '>
                 <h1 className='text-center'>Food Zone</h1>
                 <div className="row  rounded-5 m-1">
                     <div className="col-8">
-                        <Input value={food} className='bg-danger text-white' onChange={(e) => change(e)} />
+                        {/* first */}
+                        <Input value={food} className='bg-danger text-white'
+                            // second e.ma input ni value male atle
+                            onChange={(e) => change(e)} />
                     </div>
                     <div className="col-4">
-                        <Button className='bg-primary text-white'  onClick={()=>addvalue()} >Add</Button>
+                       
+                        <Button className='bg-primary text-white' onClick={() => addvalue()} >Add</Button>
                     </div>
                 </div>
             </div>
-            {foodList.length > 0 ?   <div className=' border border-5 border-primary w-25 m-5 p-2 '>
-                     {      console.log(foodList.length)} 
+            {foodList.length > 0 ? <div className=' border border-5 border-primary w-25 m-5 p-2 '>
+                {console.log(foodList.length)}
                 <h1 className='text-center'>Food List</h1>
                 <div >
-               {foodList.map((e,elemet)=>{
-                return <p>{elemet+1}{e}</p>
-               })}
-               </div>
-            </div>: <h5 className='ms-5'>Plese enter food</h5>
+                    {foodList.map((e, elemet) => {
+                        return <p>{elemet + 1}{e}</p>
+                    })}
+                </div>
+            </div> : <h5 className='ms-5'>Plese enter food</h5>
             }
-    
+
         </>
     )
 }
