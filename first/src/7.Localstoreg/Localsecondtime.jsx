f0da0f4aacbdbfa339b84196f2f9d3085e25301f
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-12-07 14:53:10 +0530

    add

--- first/src/7.Localstoreg/Localsecondtime.jsx
+++ first/src/7.Localstoreg/Localsecondtime.jsx
@@ -5,15 +5,41 @@ import { Input } from 'reactstrap'
 export default function Localsecondtime() {
     let [value, setvalue] = useState("")
     let [save, setsave] = useState([])
+    let [index, setindex] = useState(null)
     const addd = () => {
         setsave([...save, value])
         localStorage.setItem("Todo", JSON.stringify([...save, value]))
         setvalue("")
     }
-    useEffect(()=>{
-        let jsondata=localStorage.getItem("Todo")
-        setsave(jsondata || [])
-    },[])
+    useEffect(() => {
+        let jsondata = localStorage.getItem("Todo")
+        let normal = JSON.parse(jsondata)
+        setsave(normal || [])
+    }, [])
+    const delett = (i) => {
+        save.splice(i, 1)
+        setsave([...save])
+        localStorage.setItem("Todo", JSON.stringify([...save]))
+
+    }
+    const upda = (e, i) => {
+        setvalue(e)
+        setindex(i)
+
+    }
+    const updateee = () => {
+        save.splice(index, 1, value)
+        setsave([...save])
+        localStorage.setItem("Todo", JSON.stringify([...save]))
+        setvalue("")
+        setindex(null)
+
+    }
+    const Deletall= ()=>{
+       setsave([])
+        localStorage.clear()
+    }
+
     return (
         <>
 
@@ -21,7 +47,16 @@ export default function Localsecondtime() {
                 <h1 className='text-center'>Todo</h1>
                 <div className='d-flex gap-2'>
                     <Input value={value} onChange={(e) => setvalue(e?.target?.value)} />
-                    <Button onClick={() => addd()}>Add</Button>
+                    {
+                        index || index === 0 ? <Button onClick={() => updateee()}>update</Button> :
+                            <Button onClick={() => addd()}>Add</Button>
+
+                    }
+                    <Button onClick={()=>Deletall()}>Clear</Button>
+
+                    {
+                        console.log("--->", index)
+                    }
                 </div>
             </div>
             <div className='border border-dark w-25 m-auto mt-5'>
@@ -31,8 +66,12 @@ export default function Localsecondtime() {
                     {
                         save.map((e, i) => {
                             return <div key={i} className='d-flex justify-content-between mt-2'>
+                             
                                 <li>{e}</li>
-                                <Button>Delet</Button>
+
+                                <Button onClick={() => delett(i)}>Delet</Button>
+                                <Button onClick={() => upda(e, i)}>update</Button>
+
                             </div>
 
                         })
