c987268486c130aa21c460b6754a69d3e728e9fd
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-12-18 11:43:56 +0530

    add

--- first/src/2.Hook/UseReduce/UserreduceInputTask.jsx
+++ first/src/2.Hook/UseReduce/UserreduceInputTask.jsx
@@ -1,5 +1,6 @@
 import { Button } from 'reactstrap'
 import React, { useReducer, useRef, useState } from 'react'
+import { toast } from 'react-toastify'
 
 export default function UserreduceInputTask() {
     let adddiv = useRef(null)
@@ -18,14 +19,14 @@ export default function UserreduceInputTask() {
     let change = (e) => {
         setadd(e.target.value)
     }
-  
+{console.log(add)}
 
     return (
         <>
             <div className='w-25 m-auto'>
                 <input type="text" onChange={(e) => change(e)} ref={adddiv} />
                 <h1>Count is {count}</h1>
-                <Button onClick={() =>  dispatch({ actiontype: "inc", payload: add, remove: adddiv.current.value = "" }) } > ADD</Button>
+                <Button onClick={() => { add == "" ? toast.error("Please fill Input") : dispatch({ actiontype: "inc", payload: add, remove: adddiv.current.value = "" ,add:setadd("")}) }} > ADD</Button>
             </div>
         </>
     )
