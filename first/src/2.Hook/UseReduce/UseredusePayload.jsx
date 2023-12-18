68ba3632fd0d2ccabf8414b90bd1ac688e2c0640
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-12-18 12:03:21 +0530

    add

--- first/src/2.Hook/UseReduce/UseredusePayload.jsx
+++ first/src/2.Hook/UseReduce/UseredusePayload.jsx
@@ -3,23 +3,23 @@ import { Button } from 'reactstrap'
 
 export default function UseredusePayload() {
     let Reduserr = (state, action) => {
-        console.log(state, action);
         if (action.actiontype == "Inc") {
-            return { ...state,count: state.count + action.playload }
+            return { ...state, count: state.count + action.playload }
         }
         else if (action.actiontype = "Dec") {
-            return {...state, amount: state.amount + action.playload }
+            return { ...state, amount: state.amount + action.playload }
         }
     }
-    let [count, dispatch] = useReducer(Reduserr, { count: 1000,amount: 1 })
+    let [count, dispatch] = useReducer(Reduserr, { count: 1000, amount: 1 })
     return (
         <>
             <div className='m-auto w-50'>
                 <h1>Count value is '{count.count}'</h1>
                 <h1>amount value is '{count.amount}'</h1>
-
-                <Button className="w-50" onClick={() => dispatch({ actiontype: "Inc", playload: 10 })}>Add</Button>
-                <Button className="w-50" onClick={() => dispatch({ actiontype: "Dec", playload: 10 })}>Add</Button>
+                <div className='w-100 d-flex flex-column gap-3'>
+                    <Button className="w-50" onClick={() => dispatch({ actiontype: "Inc", playload: 10 })}>count</Button>
+                    <Button className="w-50" onClick={() => dispatch({ actiontype: "Dec", playload: 10 })}>Amount</Button>
+                </div>
             </div>
         </>
     )
