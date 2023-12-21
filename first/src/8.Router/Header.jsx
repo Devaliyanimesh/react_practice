86da8f8f8f5ffe9fc35bcd94a63cf2fb794c2ff7
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2023-12-21 11:23:30 +0530

    add

--- first/src/8.Router/Header.jsx
+++ first/src/8.Router/Header.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import { Link ,NavLink} from 'react-router-dom'
-import "./../App.css"
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./../App.css";
 
 export default function Header() {
   return (
     <div>
-    <ul>
-      <li>
-        {/* <Link to={"/"}>Home</Link> */}
-        <NavLink to={"/"}>Home</NavLink>
-    
-
-      </li>
-      <li>
-        {/* <Link to={"/contact"}>contact</Link> */}
-        <NavLink to={"/contact"}>contact</NavLink>
-      </li>
-    </ul>
-  </div>
-  )
+      <ul>
+        <li>
+          {/* <Link to={"/"}>Home</Link> */}
+          <NavLink to={"/"}>Home</NavLink>
+        </li>
+        <li>
+          {/* <Link to={"/contact"}>contact</Link> */}
+          <NavLink to={"/contact"}>contact</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 }
