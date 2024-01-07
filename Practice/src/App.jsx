bed1acdeb8c34293bc6ec6449e09ec8e1be6ff88
DevaliyaHardik <97013624+DevaliyaHardik@users.noreply.github.com>
2024-01-07 12:54:51 +0530

    add

--- Practice/src/App.jsx
+++ Practice/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Todo from './Input/Todo'
+import Inputvalue from './Input/Inputvalue'
 
 function App() {
 
   return (
    <>
-   <Todo/>
+   {/* <Todo/> */}
+   <Inputvalue/>
    </>
   )
 }
