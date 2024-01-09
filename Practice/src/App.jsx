110a8fd8ce2e934455c7afffef4fc7b81df32f74
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2024-01-09 12:58:04 +0530

    add

--- Practice/src/App.jsx
+++ Practice/src/App.jsx
@@ -4,16 +4,18 @@ import "./App.css";
 import Todo from "./Input/Todo";
 import Login from "./Login/login";
 import Register from "./Login/Register";
+import CheckBox from "./Input/CheckBox";
 
 function App() {
   return (
     <>
       <div>
+      <CheckBox/>
         {/* <Todo /> */}
-        <div className="d-flex gap-4 p-5">
+        {/* <div className="d-flex gap-4 p-5">
           <Register />
           <Login />
-        </div>
+        </div> */}
       </div>
     </>
   );
