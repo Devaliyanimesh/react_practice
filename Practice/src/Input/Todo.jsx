e22533e9f5ecce856fd238ab97a82485aa674721
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2024-01-12 14:39:16 +0530

    add

--- Practice/src/Input/Todo.jsx
+++ Practice/src/Input/Todo.jsx
@@ -7,6 +7,9 @@ export default function Todo() {
   let [list, setList] = useState("");
   let [addlist, setAddlist] = useState([]);
   let [transfer, setTransfer] = useState([]);
+  let [checkdata, setChekdata] = useState([]);
+  let [chek, setChek] = useState(false);
+
   const dataHandler = () => {
     if (list === "") {
       toast.error("please fill input");
@@ -35,10 +38,25 @@ export default function Todo() {
     });
     setTransfer(filter);
   };
+  const chekboxdata = (e) => {
+    let matchCheck = checkdata.includes(e);
+    if (matchCheck) {
+      let filter = checkdata.filter((ee) => ee !== e);
+      setChekdata(filter);
+    } else {
+      setChekdata([...checkdata, e]);
+    }
+  };
+  const chektrandfer = (e) => {
+    setChek(e.target.checked);
+    if (chek === true) {
+      set;
+    }
+  };
   return (
     <>
       <div className="linear w-50 m-auto bg pt-5">
-        <h4 className="text-center" style={{ color: "white" }}>
+        <h4 className="text-center" style={{ color: "black" }}>
           Just Do it..
         </h4>
 
@@ -57,7 +75,9 @@ export default function Todo() {
             className="m-auto mt-5 rounded-3 p-2 text-white p-3 "
           >
             <h5 className="text-center">To do list</h5>
-
+            <div className="d-flex justify-content-end">
+              <input type="checkbox" onChange={(e) => chektrandfer(e)} />
+            </div>
             {addlist.map((e, i) => {
               return (
                 <div
@@ -70,13 +90,29 @@ export default function Todo() {
                   }}
                   key={i}
                 >
-                  <p>
+                  <p
+                    style={
+                      chek === true ? { textDecoration: "line-through" } : null
+                    }
+                  >
                     {i + 1}. {e}
                   </p>
+                  <div className="d-flex align-items-center gap-2">
+                    <i
+                      className="fas fa-circle-plus"
+                      role="button"
+                      onClick={() => listHandler(i)}
+                    ></i>
 
-                  <p role="button" onClick={() => listHandler(i)}>
-                    <i class="fas fa-circle-plus"></i>
-                  </p>
+                    <input
+                      type="checkbox"
+                      onChange={() => chekboxdata(e)}
+                      checked={
+                        (chek === true ? true : false) ||
+                        checkdata?.includes?.(e)
+                      }
+                    />
+                  </div>
                 </div>
               );
             })}
@@ -103,10 +139,14 @@ export default function Todo() {
                     {i + 1}. {e}
                   </p>
 
-                  <p role="button" >
-                    <i class="fas fa-trash" style={{ marginRight: "10px" }} onClick={() => returnData(i)}></i>
+                  <p role="button">
                     <i
-                      class="fas fa-circle-xmark"
+                      className="fas fa-trash"
+                      style={{ marginRight: "10px" }}
+                      onClick={() => returnData(i)}
+                    ></i>
+                    <i
+                      className="fas fa-circle-xmark"
                       onClick={() => permanetDelet(i)}
                     ></i>
                   </p>
