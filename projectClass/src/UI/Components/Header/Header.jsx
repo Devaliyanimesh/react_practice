5b1fe78d5ad9a18be99bcb7f9f67f371b674ba28
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2024-01-23 12:53:22 +0530

    add

--- projectClass/src/UI/Components/Header/Header.jsx
+++ projectClass/src/UI/Components/Header/Header.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from "react";
+import log from "./../../../Images/logo.png";
+import "./Header.css";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <>
+      <div className="header">
+        <div className="logo w-25">
+          <img src={log} alt="" className="w-100" />
+        </div>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>Men</li>
+            <li>Woman</li>
+            <li>About</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
