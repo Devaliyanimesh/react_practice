97835608ed0ba03ecc0a2cfe9bfff2c0c8daa2e4
Devaliya Nimesh <devaliyanimesh61972@gmail.com>
2024-02-26 17:48:15 +0530

    add

--- ExpensesTrackers/src/Pages/InputData/TableData.jsx
+++ ExpensesTrackers/src/Pages/InputData/TableData.jsx
@@ -9,35 +9,46 @@ import img7 from "./../../img/bike.jpg";
 import img8 from "./../../img/petrole.jpg";
 import img9 from "./../../img/clothing.jpg";
 import img10 from "./../../img/grocery.jpg";
-import { Input } from "reactstrap";
-let imgg=[img1,img2,img3,img4,img5,img6,img7,img8,img9,img10]
+import { Input, Table } from "reactstrap";
+let imgg = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
 
 export default function TableData() {
   return (
     <>
-      <div className=" d-flex mt-3 gap-4 items-center px-2">
-       {
-          imgg.map((e,i)=>{
-            return  <div className=" d-flex" key={i}>
-            <div
-              className=" overflow-hidden  w-20 h-20 relative  "
-              style={{ borderRadius: "50%" }}
-              role="button"
-            >
-              <img src={e} className=" w-full h-full " alt="" />
+      <div className=" d-flex mt-3 gap-4 items-center px-2 justify-center">
+        {imgg.map((e, i) => {
+          return (
+            <div className=" d-flex" key={i}>
+              <div
+                className=" overflow-hidden  w-20 h-20 relative  "
+                style={{ borderRadius: "50%" }}
+                role="button"
+              >
+                <img src={e} className=" w-full h-full " alt="" />
+              </div>
+              <div
+                className="absolute   bg-black opacity-30 hover:opacity-0 transition duration-300 ease-in-out"
+                style={{ borderRadius: "50%", width: "5.2%", height: "11.5%" }}
+              ></div>
             </div>
-            <div
-              className="absolute   bg-black opacity-30 hover:opacity-0 transition duration-300 ease-in-out"
-              style={{ borderRadius: "50%", width: "5.2%", height: "11.5%" }}
-            ></div>
-          </div>
-          })
-        }
-       <div>
-        <Input type="date" />
-       </div>
+          );
+        })}
+        <div>
+          <Input type="date" />
+        </div>
       </div>
-      
+      <Table className="mt-2">
+        <thead>
+          <tr>
+            <th>Sr.no</th>
+            <th>Money</th>
+            <th>Discription</th>
+            <th>List</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </Table>
     </>
   );
 }
